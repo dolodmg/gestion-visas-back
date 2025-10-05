329fa8c7e5cf09abc08b637d0df1e5563674feaa
dolodmg <oranotherway@gmail.com>
2025-10-05 18:14:11 -0300

    fix cors

--- src/main/java/com/back_visas/back_visas/controller/MercadoPagoController.java
+++ src/main/java/com/back_visas/back_visas/controller/MercadoPagoController.java
@@ -18,7 +18,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/mercadopago")
-@CrossOrigin(origins = "http://localhost:3000")
 public class MercadoPagoController {
 
     private final MercadoPagoService mercadoPagoService;
