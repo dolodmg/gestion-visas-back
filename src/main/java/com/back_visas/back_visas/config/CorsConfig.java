1667bc9155bf351373a5857485530c186b112d6c
dolodmg <oranotherway@gmail.com>
2025-10-05 14:41:55 -0300

    fix: cors config

--- src/main/java/com/back_visas/back_visas/config/CorsConfig.java
+++ src/main/java/com/back_visas/back_visas/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.back_visas.back_visas.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,12 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer(@Value("${front.protocol}://${front.domain}") String frontUrl) {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(frontUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
