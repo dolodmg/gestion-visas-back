3a8d557f65eacef238e613de69b2008e627f0023
dolodmg <oranotherway@gmail.com>
2025-10-06 19:38:25 -0300

    fix en controller de contact

--- src/main/java/com/back_visas/back_visas/controller/ContactController.java
+++ src/main/java/com/back_visas/back_visas/controller/ContactController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/contact")
 public class ContactController {
 
-    @Value("${MAIL}")
+    @Value("${spring.mail.username}")
     private String fromMail;
 
     private final JavaMailSender mailSender;
